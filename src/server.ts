573928defa8b5a0d1e9bd6a7d9ca4d3c4495b4e2
bruno-fialho <brugerf@gmail.com>
2021-03-15 11:23:43 -0300

    Add debug config

--- src/server.ts
+++ src/server.ts
@@ -8,4 +8,10 @@ app.get("/", (request, response) => {
   return response.json({ message: "Hello World" });
 });
 
+app.post("/courses", (request, response) => {
+  const { name } = request.body;
+
+  return response.json({ name });
+});
+
 app.listen(3333, () => console.log("Server is running!"));

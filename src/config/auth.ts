ddedf62b75fc1d376fb6cf597afd64f823a0eaba
bruno-fialho <brugerf@gmail.com>
2021-04-05 16:06:16 -0300

    Refactor authenticate user

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: "1e5b96d087b10aa17668f74903ee7a7d",
+  secret_refresh_token: "d1461ddaa0e0a7b3dc8339b23ce9b5fa",
+  expires_in_token: "15m",
+  expires_in_refresh_token: "30d",
+  expires_refresh_token_days: 30,
+};
--- src/modules/accounts/infra/typeorm/repositories/UsersTokensRepository.ts
+++ src/modules/accounts/infra/typeorm/repositories/UsersTokensRepository.ts
@@ -5,7 +5,7 @@ import { IUsersTokensRepository } from "@modules/accounts/repositories/IUsersTok
 
 import { UserTokens } from "../entities/UserTokens";
 
-class UsersTokenRepository implements IUsersTokensRepository {
+class UsersTokensRepository implements IUsersTokensRepository {
   private repository: Repository<UserTokens>;
 
   constructor() {
@@ -29,4 +29,4 @@ class UsersTokenRepository implements IUsersTokensRepository {
   }
 }
 
-export { UsersTokenRepository };
+export { UsersTokensRepository };

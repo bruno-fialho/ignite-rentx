1ae7937872aa852b457d19216aaf17b1d603fbfb
bruno-fialho <brugerf@gmail.com>
2021-04-05 10:28:51 -0300

    Add devolution use case and controller

--- src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
+++ src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
@@ -9,6 +9,7 @@ class DayjsDateProvider implements IDateProvider {
   compareInHours(start_date: Date, end_date: Date): number {
     const end_date_utc = this.convertToUtc(end_date);
     const start_date_utc = this.convertToUtc(start_date);
+
     return dayjs(end_date_utc).diff(start_date_utc, "hours");
   }
 
@@ -18,6 +19,13 @@ class DayjsDateProvider implements IDateProvider {
 
   dateNow(): Date {
     return dayjs().toDate();
+  }
+
+  compareInDays(start_date: Date, end_date: Date): number {
+    const end_date_utc = this.convertToUtc(end_date);
+    const start_date_utc = this.convertToUtc(start_date);
+
+    return dayjs(end_date_utc).diff(start_date_utc, "days");
   }
 }
 

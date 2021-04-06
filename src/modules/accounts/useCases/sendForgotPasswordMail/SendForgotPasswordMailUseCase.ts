bf507759568b83855a894b3e5278e3890c7df7d0
bruno-fialho <brugerf@gmail.com>
2021-04-06 10:01:28 -0300

    Add Ethereal mail provider

--- src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
+++ src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
@@ -4,6 +4,7 @@ import { v4 as uuidV4 } from "uuid";
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
 import { IUsersTokensRepository } from "@modules/accounts/repositories/IUsersTokensRepository";
 import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
+import { IMailProvider } from "@shared/container/providers/MailProvider/IMailProvider";
 import { AppError } from "@shared/errors/AppError";
 
 @injectable()
@@ -16,9 +17,12 @@ class SendForgotPasswordMailUseCase {
     private usersTokensRepository: IUsersTokensRepository,
 
     @inject("DayjsDateProvider")
-    private dateProvider: IDateProvider
+    private dateProvider: IDateProvider,
+
+    @inject("EtherealMailProvider")
+    private mailProvider: IMailProvider
   ) {}
-  async execute(email: string) {
+  async execute(email: string): Promise<void> {
     const user = await this.usersRepositoru.findByEmail(email);
 
     if (!user) {
@@ -34,6 +38,12 @@ class SendForgotPasswordMailUseCase {
       user_id: user.id,
       expires_date,
     });
+
+    await this.mailProvider.sendMail(
+      email,
+      "Recuperação de senha",
+      `O link para o reset é ${token}`
+    );
   }
 }
 

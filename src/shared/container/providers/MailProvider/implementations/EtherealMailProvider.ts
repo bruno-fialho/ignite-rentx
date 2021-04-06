3a3c39a6e8cc54dcf729de799e72f2cd1160e2ad
bruno-fialho <brugerf@gmail.com>
2021-04-06 10:39:03 -0300

    Add email template

--- src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
+++ src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -1,7 +1,14 @@
+import fs from "fs";
+import handlebars from "handlebars";
 import nodemailer, { Transporter } from "nodemailer";
 import { injectable } from "tsyringe";
 
 import { IMailProvider } from "../IMailProvider";
+
+interface IVariables {
+  name: string;
+  link: string;
+}
 
 @injectable()
 class EtherealMailProvider implements IMailProvider {
@@ -26,13 +33,23 @@ class EtherealMailProvider implements IMailProvider {
       .catch((err) => console.log(err));
   }
 
-  async sendMail(to: string, subject: string, body: string): Promise<void> {
+  async sendMail(
+    to: string,
+    subject: string,
+    variables: IVariables,
+    path: string
+  ): Promise<void> {
+    const templateFileContent = fs.readFileSync(path).toString("utf-8");
+
+    const templateParse = handlebars.compile(templateFileContent);
+
+    const templateHTML = templateParse(variables);
+
     const message = await this.client.sendMail({
       to,
       from: "Rentx <noreply@rentx.com.br>",
       subject,
-      text: body,
-      html: body,
+      html: templateHTML,
     });
 
     console.log("Message sent: %s", message.messageId);

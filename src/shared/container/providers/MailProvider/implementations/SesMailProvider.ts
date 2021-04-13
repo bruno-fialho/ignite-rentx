6cd46db1abfc910a60d5fcca16c3e45cd4a9e60b
bruno-fialho <brugerf@gmail.com>
2021-04-12 21:33:09 -0300

    Add SESMail provider

---
+++ src/shared/container/providers/MailProvider/implementations/SesMailProvider.ts
@@ -0,0 +1,43 @@
+import { SES } from "aws-sdk";
+import fs from "fs";
+import handlebars from "handlebars";
+import nodemailer, { Transporter } from "nodemailer";
+import { injectable } from "tsyringe";
+
+import { IMailProvider, IVariables } from "../IMailProvider";
+
+@injectable()
+class SESMailProvider implements IMailProvider {
+  private client: Transporter;
+
+  constructor() {
+    this.client = nodemailer.createTransport({
+      SES: new SES({
+        apiVersion: "2010-12-01",
+        region: process.env.AWS_REGION,
+      }),
+    });
+  }
+
+  async sendMail(
+    to: string,
+    subject: string,
+    variables: IVariables,
+    path: string
+  ): Promise<void> {
+    const templateFileContent = fs.readFileSync(path).toString("utf-8");
+
+    const templateParse = handlebars.compile(templateFileContent);
+
+    const templateHTML = templateParse(variables);
+
+    await this.client.sendMail({
+      to,
+      from: "Rentx <bruno@fialho.me>",
+      subject,
+      html: templateHTML,
+    });
+  }
+}
+
+export { SESMailProvider };
